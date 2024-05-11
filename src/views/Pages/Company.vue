56102476f1a14a8f6f0ba0cb7285009a1b4c8ca1
GhulamMohiudinN <mohiudin7174@gmail.com>
2024-05-11 18:26:00 +0500

    company page

--- src/views/Pages/Company.vue
+++ src/views/Pages/Company.vue
@@ -9,7 +9,7 @@
         <img class="w-70 sm:w-90" src="@/assets/vueJs/logoDark.png" alt="" />
       </div>
       <div
-        class="border-2 border-[black] bg-[#E6E6E7] text-black mt-16 w-[90%] sm:w-[435px] lg:w-[700px] mx-auto rounded-[50px] lg:rounded-[80px]"
+        class="border-2 border-[black] bg-[#E6E6E7] text-black mt-12 w-[90%] sm:w-[435px] lg:w-[650px] mx-auto rounded-[50px] lg:rounded-[80px]"
       >
         <div>
           <p
@@ -17,7 +17,7 @@
           >
             Seleccionar Empresa
           </p>
-          <div class="lg:pb-10 pb-5">
+          <div class="lg:pb-0 pb-5">
             <select
               class="bg-[#E6E6E7] lg:text-[20px] border-b border-[#aaaeb6] w-[80%] lg:w-[440px] ml-[10%] lg:ml-32 text-black-2 focus:outline-none pt-3 mb-17"
             >
@@ -34,7 +34,7 @@
           </div>
         </div>
         <div
-          class="w-fit mx-auto lg:ml-[450px] md:ml-[270px] sm:mb-[-20px] mb-[-7%]"
+          class="w-fit mx-auto lg:ml-[400px] md:ml-[270px] sm:mb-[-20px] mb-[-7%]"
         >
           <button
             class="text-[#62666F] font-extralight bg-[#E6E6E7] border border-black py-2 px-15 lg:px-16 sm:px-6 rounded-full text-2xl"
