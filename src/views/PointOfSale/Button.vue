5c461697fdb814af595e22de637de149f99abc04
chzeeshan099 <chzeeshanszforever099@gmail.com>
2024-05-11 20:42:46 +0500

    fixed build issue

--- src/views/PointOfSale/Button.vue
+++ src/views/PointOfSale/Button.vue
@@ -1,8 +1,10 @@
 <template>
-    <button class="text-white bg-[#62666F] py-2 px-10 rounded-[20px] text-xl">Igresar</button>
-</template>
-<script>
-export default{
-    name:"Button"
-}
-</script>
+    <button class="text-white bg-[#62666F] py-2 px-10 rounded-[20px] text-xl">Ingresar</button>
+  </template>
+  
+  <script setup lang="ts">
+  // No need to export default in script setup
+  // Name can be set directly as a variable
+  const name = "Button";
+  </script>
+  
--- src/views/PointOfSale/Card.vue
+++ src/views/PointOfSale/Card.vue
@@ -1,30 +1,37 @@
 <template>
-<div class="sm:flex sm:flex-wrap ">
-    <div  v-for="item in dataItems" :key="item.id" class="dark:shadow[ 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);] lg:w-[270px] w-[230px] mb-10 mx-auto text-center py-10 rounded-[20px] dark:bg-[#363B3D]" style=" box-shadow: 0 3px 7px rgba(0,0,0,0.36), 0 4px 8px rgba(0,0,0,0.53);">
+    <div class="sm:flex sm:flex-wrap">
+      <div
+        v-for="item in dataItems"
+        :key="item.id"
+        class="dark:shadow-[0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)] lg:w-[270px] w-[230px] mb-10 mx-auto text-center py-10 rounded-[20px] dark:bg-[#363B3D]"
+        style="box-shadow: 0 3px 7px rgba(0,0,0,0.36), 0 4px 8px rgba(0,0,0,0.53);"
+      >
         <h1 class="pb-8 text-4xl font-light">{{ item.name }}</h1>
         <p>{{ item.data }}</p>
         <p>{{ item.number }}</p>
         <p>{{ item.email }}</p>
         <div class="mb-[-60px] pt-16">
-            <Button />
+          <Button />
+        </div>
+      </div>
     </div>
-    </div>
-</div>
-
-</template>
-
-<script>
-import Button from '@/views/PointOfSale/Button.vue'
-export default {
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  import Button from '@/views/PointOfSale/Button.vue';
+  
+  export default defineComponent({
     name: "Card",
     components: {
-        Button
+      Button
     },
     props: {
-        dataItems: {
-            type: Array,
-            required: true
-        }
+      dataItems: {
+        type: Array,
+        required: true
+      }
     }
-}
-</script>
+  });
+  </script>
+  
